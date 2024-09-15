a8b9815b9c1193b7e078a22c0bd1ee3bde3edff2
Edward Coristine <edward.coristine@neuralink.com>
2024-09-15 12:43:26 -0400

    include build.zig.zon

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,8 @@ VERSION=$1
 zig build test
 zig build
 
-zip -r dotenv-$VERSION.zip src/ zig-out/
+zip -r dotenv-$VERSION.zip src/ zig-out/ build.zig.zon
 echo "Created dotenv-$VERSION.zip"
 
-tar -czvf dotenv-$VERSION.tar.gz src/ zig-out/
+tar -czvf dotenv-$VERSION.tar.gz src/ zig-out/ build.zig.zon
 echo "Created dotenv-$VERSION.tar.gz"
